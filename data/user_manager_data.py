2d272b4523735a92772084e9622f50eaf62aefec
wangguangxue159 <wangguangxue159@163.com>
2021-04-19 23:18:00 +0800

    Initial commit

---
+++ data/user_manager_data.py
@@ -0,0 +1,8 @@
+
+class UserManagerData():
+
+    user_case1_data = {
+        "username":"testk03",
+        "password":"testk03",
+        "ner_user":"testk03"
+    }
---
+++ utils/common_utils.py
@@ -0,0 +1,13 @@
+
+
+#实现公共断言
+
+def common_assert(obj,result,except_result=0):
+    obj.assertEqual(except_result,result['errno'])
+
+
+
+
+
+
+
